330420c65d63632be5b99e1dc6fe7d713c382c93
DevenairThomas <deventhomas55@gmail.com>
2023-02-08 22:53:25 -0500

    Add project files.

---
+++ EbayScraperWPF/AddItemWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EbayScraperWPF
+{
+    /// <summary>
+    /// Interaction logic for Window2.xaml
+    /// </summary>
+    public partial class AddItemWindow : Window
+    {
+        public AddItemWindow()
+        {
+            InitializeComponent();
+        }
+        public string itemName;
+        public string itemKeywords;
+        public double itemMaxPrice;
+        public double itemLowPrice;
+        public bool itemAuction;
+        public bool itemBuyNow;
+        public bool itemOffer;
+
+        static EbayItem ebayitem;
+        private void btnSaveItem_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void textLowPrice_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void textMaxPrice_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void textItemName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void textItemKeywords_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void checkboxAuction_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void checkboxBuyNow_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void checkboxOffer_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ EbayScraperWPF/EbayItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EbayScraperWPF
+{
+    interface IItem
+    {
+        string Name { get; set; }
+        string Keywords { get; set; }
+        double MaxPrice { get; set; }
+        double LowPrice { get; set; }
+        bool Auction { get; set; }
+        bool BuyNow { get; set; }
+        bool Offer { get; set; }
+    }
+    class EbayItem : IItem
+    {
+        public string Name { get; set; }
+        public string Keywords { get; set; }
+        public double MaxPrice { get; set; }
+        public double LowPrice { get; set; }
+        public bool Auction { get; set; }
+        public bool BuyNow { get; set; }
+        public bool Offer { get; set; }
+    }
+}
