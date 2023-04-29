9737120b8a78e09cf7ca6caf1264bdc2e6f805bb
deven <deven@DESKTOP-M3ELVTG>
2023-04-28 22:18:21 -0400

    #No message

---
+++ EbayScraperWPF/Model/EbayItemData.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EbayScraperWPF.Model
+{
+    public class EbayItemData
+    {
+        private string _ItemName;
+        private FindEbayItemData _FindEbayItemData;
+        private int _TimesPolled;
+        private int _RecordsInDatabase;
+        private UserData _UserData;
+        private DateTime _DatePolled;
+        private DateTime _DateCreated;
+        private int _EbayItemDataID;
+
+        public EbayItemData()
+        {
+            _FindEbayItemData = new FindEbayItemData();
+            _UserData = new UserData();
+        }
+
+        public string ItemName
+        {
+            get { return _ItemName; }
+            set { _ItemName = value; }
+        }
+        public int TimesPolled
+        {
+            get { return _TimesPolled; }
+            set { _TimesPolled = value; }
+        }
+        public FindEbayItemData FindEbayItemData 
+        {
+            get { return _FindEbayItemData;} 
+            set { _FindEbayItemData = value;}
+        }
+        public int RecordsInDatabase
+        {
+            get { return _RecordsInDatabase; }
+            set { _RecordsInDatabase = value; }
+        }
+        public DateTime DateCreated
+        {
+            get { return _DateCreated; }
+            set { _DateCreated = value; }
+        }
+        public DateTime DatePolled 
+        { 
+            get { return _DatePolled; }
+            set { _DatePolled = value; } 
+        }
+        public UserData UserData 
+        { 
+            get { return _UserData; }
+            set { _UserData = UserData; }
+        }
+        public int EbayItemDataID
+        {
+            get { return _EbayItemDataID; }
+            set { _EbayItemDataID = value; }
+        }
+
+    }
+}
--- EbayScraperWPF/ViewModel/HomeViewModel.cs
+++ EbayScraperWPF/ViewModel/HomeViewModel.cs
@@ -1,13 +1,95 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace EbayScraperWPF.ViewModel
 {
     public class HomeViewModel : ViewModelBase
     {
         public ViewModelBase CurrentViewModel { get; }
+        private UserData _UserData;
+
+        private string _UserEmail;
+        private string _UserPassword;
+
+        public enum PasswordStates
+        {
+            None,
+            EMAIL_CHECK_SUCCESS,
+            PASSWORD_CHECK_SUCCESS,
+            USER_CHECK_SUCCESS,
+            EMAIL_CHECK_FAIL,
+            USER_CHECK_FAIL,
+            PASSWORD_CHECK_FAIL
+        }
+        
+        public HomeViewModel()
+        {
+           _UserData = new UserData();
+        }
+        public UserData UserData 
+        { 
+            get { return _UserData; } 
+        }
+        public string UserEmail
+        {
+            get { return _UserEmail; }
+            set
+            {
+                _UserEmail = value;
+                OnPropertyChanged();
+            }
+        }
+        public string UserPassword
+        {
+            get { return _UserPassword; }
+            set
+            {
+                _UserPassword = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public PasswordStates CheckLogin(string enteredUserName,string storedUserName, string enteredUserPassword, string storedUserPassword)
+        {
+            PasswordStates passwordStates = PasswordStates.None;
+            //No User password entered
+            if (string.IsNullOrEmpty(enteredUserPassword)) 
+            {
+                MessageBox.Show("message", nameof(enteredUserPassword));
+                passwordStates = PasswordStates.USER_CHECK_FAIL;
+                return passwordStates;
+            }
+            //No User Name entered
+            else if (string.IsNullOrEmpty(enteredUserName))
+            {
+                MessageBox.Show("message", nameof(enteredUserName));
+                passwordStates = PasswordStates.USER_CHECK_FAIL;
+                return passwordStates;
+            }
+            //User Name Entered Success
+            else if (enteredUserName == storedUserName)
+            {
+                //User Password Entered Success
+                passwordStates = PasswordStates.EMAIL_CHECK_SUCCESS;
+                if(enteredUserPassword == storedUserPassword)
+                {
+                    passwordStates = PasswordStates.PASSWORD_CHECK_SUCCESS;
+                    return passwordStates;
+                }
+                return PasswordStates.PASSWORD_CHECK_FAIL;
+            }
+            else
+            {
+                MessageBox.Show("message", nameof(enteredUserPassword));
+                passwordStates = PasswordStates.EMAIL_CHECK_FAIL;
+            }
+            return passwordStates;
+        }
+        
     }
 }
